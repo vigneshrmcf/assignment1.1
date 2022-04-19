2dc8c53b385563f49b88cbf12fb093f187e81346
vigneshrmcf <102026466+vigneshrmcf@users.noreply.github.com>
2022-04-19 12:12:53 +0530

    Add files via upload

---
+++ edyodaassignment5.py
@@ -0,0 +1,13 @@
+class square:
+   def power(self, x, n):
+        if x==0 or x==1 or n==1:
+            return x 
+
+        val = self.power(x,n//2)
+        if n%2 ==0:
+            return val*val
+        return val*val*x
+
+x=int(input("Enter x value :"))
+n=int(input("Enter n value :"))
+print("pow(x,n) value is :",square().power(x,n))
